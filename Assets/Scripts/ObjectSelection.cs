83c20d97aaa882899b3a9256b73226e6c84d0bfa
BuildTools <unconfigured@null.spigotmc.org>
2020-04-14 01:15:19 +0300

    committing to push

--- Assets/Scripts/ObjectSelection.cs
+++ Assets/Scripts/ObjectSelection.cs
@@ -6,6 +6,7 @@ public class ObjectSelection : MonoBehaviour
 {
     public GameObject Cam;
     public Material SelectedObjectMaterial;
+    [HideInInspector]
     public GameObject selectedObject;
     private Material originalSelectedObjectMaterial;
     public bool LockMode;
